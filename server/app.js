abad6ddd0c3ea8b26ce12e1a0ec4b65b6acb1d69
wangtongmeng <tongmengwang@163.com>
2020-03-10 22:11:40 +0800

    feat: server项目优化

--- server/app.js
+++ server/app.js
@@ -1,29 +1,10 @@
 const express = require('express')
+const router = require('./router')
 
 // 创建 express 应用
 const app = express()
 
-const myLogger = function(req, res, next) {
-  console.log('myLogger')
-  next()
-}
-
-app.use(myLogger)
-
-// 监听 / 路径的 get 请求
-app.get('/', function(req, res) {
-  // res.send('hello node')
-  throw new Error('something has error...')
-})
-
-const errorHandler = function(err, req, res, next) {
-  res.status(400).json({
-    error: -1,
-    msg: err.toString()
-  })
-}
-
-app.use(errorHandler)
+app.use('/', router)
 
 // 使 express 监听 5000 端口号发起的 http 请求
 const server = app.listen(5000, function() {
